eb7d0e7b02cc4cc2746a89e681eaabd29968e38c
kimthompson <bykimthompson@gmail.com>
2019-07-12 22:07:59 -0500

    built the darn thing

--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -5,21 +5,3 @@ menuBtn.addEventListener("click", function() {
   menu.classList.toggle("hide");
   menu.classList.toggle("show");
 });
-
-let flipCards = document.querySelectorAll(".flip_card");
-
-// console.log(flipCards);
-
-// flipCards.forEach(function(elem) {
-// let front = elem.querySelectorAll(".flip_card-front")[0];
-// let back = elem.querySelectorAll(".flip_card-back")[0];
-
-// console.log(back);
-
-// elem.addEventListener("onmouseover", function() {
-//   console.log("mouseover");
-
-// front.classList.toggle("hide");
-// back.classList.toggle("hide");
-//   });
-// });
